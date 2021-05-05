588ba823d781aa38cbe8397bd43a2c609d7adacf
evertonpinheiroti <evertonpinheiroti@gmail.com>
2021-05-05 19:54:47 -0300

    :sparkles: Criminal code listing screen created

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 import {
   Container,
   Content,
@@ -10,54 +10,469 @@ import {
   Tag,
   Divider,
   TitleStatus,
-  InformationPenal
+  InformationPenal,
+  ContentCard,
+  Loading,
+  ModalStyled,
 } from './styles';
 
-import { Header } from '../../components/Header';
-import { RiAddFill, RiAlarmLine, RiDoorLockBoxFill, RiMoneyDollarCircleLine, RiMore2Fill } from 'react-icons/ri';
+import { v4 as uuidv4 } from 'uuid';
 
-export function Dashboard() {
+import { connect } from 'react-redux';
+
+import * as actionsPenal from '../../store/actions/penalcode';
+
+import Header from '../../components/Header';
+import {
+  RiAddFill,
+  RiAlarmLine,
+  RiDeleteBin2Fill,
+  RiDoorLockBoxFill,
+  RiEditFill,
+  RiFileList3Fill,
+  RiMoneyDollarCircleLine,
+  RiSpyFill
+} from 'react-icons/ri';
+import api from '../../services/api';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErros';
+import { Input } from '../../components/Input';
+import Spinner from '../../components/Spinner';
+import { Button } from '../../components/Button';
+import { toast } from 'react-toastify';
+
+interface PenalProps {
+  penal: {
+    codes: Object
+  }
+}
+
+interface FormAttributes {
+  id: string,
+  nome: string,
+  descricao: string,
+  multa: string,
+  tempoPrisao: string,
+  status: boolean,
+}
+
+function Dashboard({ fetch, codePenals, add, remove, edit, orderActive, orderInactive }: any) {
+  const formRef = useRef<FormHandles>(null);
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenEdit, setIsOpenEdit] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [status, setStatus] = useState(1);
+
+  const [idEdit, setIdEdit] = useState<number | string>();
+
+  const [nome, setNome] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [multa, setMulta] = useState('');
+  const [tempoPrisao, setTempoPrisao] = useState('');
+
+  const [filteredStatus, setFilteredStatus] = useState({
+    actived: false,
+    inactived: false,
+  });
+
+  const [isChecked, setIsChecked] = useState(true);
+
+
+
+  useEffect(() => {
+    if (filteredStatus.actived === true || filteredStatus.inactived === true) {
+      return
+    }
+    async function obterPenal() {
+      setLoading(true)
+      const response = await api.get('/codigopenal')
+      fetch(response.data)
+      setLoading(false)
+    }
+    obterPenal();
+  }, [fetch, filteredStatus])
+
+  const filteredActive = useCallback((value: string) => {
+    if (value === '1') {
+      setFilteredStatus({ ...filteredStatus, actived: true, inactived: false })
+      orderActive()
+    } else if (value === '2') {
+      setFilteredStatus({ ...filteredStatus, inactived: true, actived: false })
+      orderInactive()
+    }
+
+  }, [filteredStatus, orderActive, orderInactive])
+
+  function toggleButtonActive() {
+    setIsChecked(true)
+    setStatus(1)
+  }
+
+  function toggleButtonInactive() {
+    setIsChecked(false)
+    setStatus(2)
+  }
+
+  function handleRemovePenal(id: string) {
+    remove(id)
+  }
+
+  const handleEditPenal = useCallback(
+    async (data: FormAttributes) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          nome: Yup.string()
+            .required('O nome é obrigatório'),
+          descricao: Yup.string()
+            .required('A descrição é obrigatório'),
+          multa: Yup.string()
+            .required('A multa é obrigatória'),
+          tempoPrisao: Yup.string()
+            .required('O tempo de prisão é obrigatório'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        const id = idEdit
+
+        const dados = {
+          id,
+          nome: data.nome,
+          descricao: data.descricao,
+          multa: data.multa,
+          tempoPrisao: data.tempoPrisao,
+          status: status,
+        }
+
+        edit(
+          dados,
+          id
+        )
+        setIsOpenEdit(false);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+      }
+    }, [edit, idEdit, status]
+  )
+
+  function handleEditModal(id: number) {
+    setIsOpenEdit(true);
+    const codeEdit = codePenals.find((penal: any) => penal.id === id)
+    setIdEdit(id);
+    setNome(codeEdit.nome);
+    setDescricao(codeEdit.descricao);
+    setMulta(codeEdit.multa);
+    setTempoPrisao(codeEdit.tempoPrisao);
+    setStatus(codeEdit.status);
+  }
+
+  const handleSubmit = useCallback(
+    async (data: FormAttributes) => {
+      try {
+        formRef.current?.setErrors({});
+        const schema = Yup.object().shape({
+          nome: Yup.string()
+            .required('O nome é obrigatório'),
+          descricao: Yup.string()
+            .required('A descrição é obrigatório'),
+          multa: Yup.string()
+            .required('A multa é obrigatória'),
+          tempoPrisao: Yup.string()
+            .required('O tempo de prisão é obrigatório'),
+        });
+
+        await schema.validate(data, {
+          abortEarly: false,
+        });
+        const dados = {
+          id: uuidv4(),
+          nome: data.nome,
+          descricao: data.descricao,
+          multa: data.multa,
+          tempoPrisao: data.tempoPrisao,
+          status,
+        }
+        add(
+          dados
+        )
+        toast.success('☑ Novo código penal adicionado!')
+        setIsOpen(false);
+      } catch (err) {
+        if (err instanceof Yup.ValidationError) {
+          const errors = getValidationErrors(err);
+          formRef.current?.setErrors(errors);
+
+          return;
+        }
+      }
+    },
+    [add, status],
+  );
+
   return (
-    <Container>
-      <Header />
-      <Content>
-        <ContentHeader>
-          <div>
-            <span>
-              Códigos penais
-            </span>
-          </div>
-          <button type="button">
-            <RiAddFill size={24} /> Adicionar código penal
-          </button>
-        </ContentHeader>
-
-        <Card>
-          <CardTop>
-            <RiDoorLockBoxFill size={36} fill="#F9B036" />
-            <RiMore2Fill size={26} />
-          </CardTop>
-          <CardTitle>
-            <TitleStatus>
+    <>
+      <Container>
+        <Header />
+        <Content>
+          <ContentHeader>
+            <div>
               <span>
-                Dinheiro Ilícitio
+                Códigos penais
               </span>
-              <Tag>
+              <p>
+                Ordene por
+              </p>
+              <select
+                onChange={(e) => filteredActive(e.target.value)}
+              >
+                <option>
+                  Status
+                </option>
+                <option value="1">
+                  Ativo
+                </option>
+                <option value="2">
+                  Inativo
+                </option>
+              </select>
+            </div>
+            <button type="button" onClick={() => setIsOpen(true)}>
+              <RiAddFill size={24} /> Adicionar código penal
+            </button>
+          </ContentHeader>
+          {loading && (
+            <Loading>
+              <p>Aguarde enquanto carregamos os dados</p>
+              <Spinner />
+            </Loading>
+          )}
+          <ContentCard>
+            {codePenals.length > 0 && codePenals.map((penal: any, index: any) => (
+              <Card key={penal.id} style={{ animationDelay: `${index / 5}s` }}>
+                <CardTop>
+                  <RiDoorLockBoxFill size={36} />
+                  <div>
+                    <button type="button" onClick={() => handleRemovePenal(penal.id)}>
+                      <RiDeleteBin2Fill size={26} />
+                    </button>
+                    <button type="button" onClick={() => handleEditModal(penal.id)}>
+                      <RiEditFill size={26} />
+                    </button>
+                  </div>
+                </CardTop>
+                <CardTitle>
+                  <TitleStatus>
+                    <span>
+                      {penal.nome}
+                    </span>
+                    <Tag status={penal.status}>
+                      {penal.status === 1 ? 'Ativo' : 'Inativo'}
+                    </Tag>
+                  </TitleStatus>
+                  <InformationPenal>
+                    <p><RiMoneyDollarCircleLine size={20} /> R$ {penal.multa}</p>
+                    <p><RiAlarmLine size={20} /> {penal.tempoPrisao} minutos</p>
+                  </InformationPenal>
+                </CardTitle>
+                <Divider />
+                <CardDescription>
+                  <p>{penal.descricao}</p>
+                </CardDescription>
+                <div className="cardHoverDescription">
+                  {penal.descricao}
+                </div>
+              </Card>
+            ))}
+          </ContentCard>
+        </Content>
+      </Container>
+
+      { isOpen && (
+        <ModalStyled
+          isOpen={isOpen}
+          checked={isChecked}
+          onRequestClose={() => setIsOpen(false)}
+          ariaHideApp={false}
+          style={{
+            content: {
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+              background: "#F0F0F5",
+              borderRadius: "8px",
+              border: "none",
+              padding: "40px",
+            },
+            overlay: {
+              backgroundColor: "#121214e6",
+            },
+          }}
+        >
+          <Form onSubmit={handleSubmit} className="formLogin" ref={formRef}>
+            <h2>Adicionar novo código penal</h2>
+            <Input
+              label="Nome"
+              name="nome"
+              id="nome"
+              placeholder="Digite um nome"
+              type="text"
+              icon={RiSpyFill}
+            />
+            <Input
+              label="Descrição"
+              name="descricao"
+              id="descricao"
+              placeholder="Digite uma descrição"
+              type="text"
+              icon={RiFileList3Fill}
+            />
+            <Input
+              label="Multa"
+              name="multa"
+              id="multa"
+              placeholder="Digite o valor da multa"
+              type="number"
+              icon={RiMoneyDollarCircleLine}
+            />
+            <Input
+              label="Tempo"
+              name="tempoPrisao"
+              id="tempoPrisao"
+              placeholder="Digite o tempo de prisão"
+              type="number"
+              icon={RiAlarmLine}
+            />
+            <div className="buttonStatus">
+              <button className="activeButton" type="button" onClick={() => toggleButtonActive()}>
                 Ativo
-              </Tag>
-            </TitleStatus>
-            <InformationPenal>
-              <p><RiMoneyDollarCircleLine size={20} /> R$ 501.1</p>
-              <p><RiAlarmLine size={20} /> 30 minutos</p>
-            </InformationPenal>
-          </CardTitle>
-          <Divider />
-          <CardDescription>
-            <p>Estar em posse de dinheiro não declarado ou de natureza ilícita
-            sem procedência. Agrava-se penalmente...</p>
-          </CardDescription>
-        </Card>
-      </Content>
-    </Container>
+              </button>
+              <button className="inactiveButton" type="button" onClick={() => toggleButtonInactive()}>
+                Inativo
+              </button>
+            </div>
+            <Button type="submit">
+              Adicionar
+            </Button>
+          </Form>
+        </ModalStyled>
+      )}
+
+      { isOpenEdit && (
+        <ModalStyled
+          checked={isChecked}
+          isOpen={isOpenEdit}
+          onRequestClose={() => setIsOpenEdit(false)}
+          ariaHideApp={false}
+          style={{
+            content: {
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              right: "auto",
+              bottom: "auto",
+              marginRight: "-50%",
+              transform: "translate(-50%, -50%)",
+              background: "#F0F0F5",
+              borderRadius: "8px",
+              border: "none",
+              padding: "40px",
+            },
+            overlay: {
+              backgroundColor: "#121214e6",
+            },
+          }}
+        >
+          <Form onSubmit={handleEditPenal} className="formLogin" ref={formRef}>
+            <h2>Editar código penal</h2>
+            <Input
+              label="Nome"
+              name="nome"
+              id="nome"
+              placeholder="Digite um nome"
+              type="text"
+              value={nome}
+              onChange={(e) => (setNome(e.target.value))}
+              icon={RiSpyFill}
+            />
+            <Input
+              label="Descrição"
+              name="descricao"
+              id="descricao"
+              placeholder="Digite uma descrição"
+              type="text"
+              value={descricao}
+              onChange={(e) => (setDescricao(e.target.value))}
+              icon={RiFileList3Fill}
+            />
+            <Input
+              label="Multa"
+              name="multa"
+              id="multa"
+              placeholder="Digite o valor da multa"
+              type="number"
+              value={multa}
+              onChange={(e) => (setMulta(e.target.value))}
+              icon={RiMoneyDollarCircleLine}
+            />
+            <Input
+              label="Tempo"
+              name="tempoPrisao"
+              id="tempoPrisao"
+              placeholder="Digite o tempo de prisão"
+              type="number"
+              value={tempoPrisao}
+              onChange={(e) => (setTempoPrisao(e.target.value))}
+              icon={RiAlarmLine}
+            />
+            <div className="buttonStatus">
+              <button className="activeButton" type="button" onClick={() => toggleButtonActive()}>
+                Ativo
+              </button>
+              <button className="inactiveButton" type="button" onClick={() => toggleButtonInactive()}>
+                Inativo
+              </button>
+            </div>
+            <Button type="submit">
+              Salvar alterações
+            </Button>
+          </Form>
+        </ModalStyled>
+      )}
+    </>
   )
 }
+
+const mapStateToProps = (state: PenalProps) => ({
+  codePenals: state.penal.codes
+});
+
+const mapDispatchToProps = (dispatch: any) => ({
+  fetch: (code: any) =>
+    dispatch(actionsPenal.fetchCodePenal(code)),
+  add: (code: any) =>
+    dispatch(actionsPenal.addCodePenal(code)),
+  edit: (code: any, id: number) =>
+    dispatch(actionsPenal.editCodePenal(code, id)),
+  remove: (id: string) =>
+    dispatch(actionsPenal.removeCodePenal(id)),
+  orderActive: () =>
+    dispatch(actionsPenal.orderActivePenal()),
+  orderInactive: () =>
+    dispatch(actionsPenal.orderInactivePenal())
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard)
--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,9 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import Modal from 'react-modal';
+
+interface TagProps {
+  status: number;
+}
 
 export const Container = styled.div`
   width: 100%;
@@ -7,14 +12,56 @@ export const Container = styled.div`
 
 export const Content = styled.div`
   width: 100%;
+  
 `;
 
 export const ContentHeader = styled.div`
   display: flex;
-  flex-direction: row;
   justify-content: space-between;
   align-items: center;
   padding: 3rem 3rem 1rem 3rem;
+
+  @keyframes animationContentHeader{
+    from {  
+      opacity: 0;
+      transform: translateY(-50px);
+    }
+    to {
+      opacity: 1;
+      transform: translateY(0);
+    }
+  }
+  animation: animationContentHeader .6s;
+
+  div {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+
+    p {
+      margin-left: 2rem;
+      font-weight: 500;
+    }
+  }
+
+  select {
+    margin-left: 0.4rem;
+    background: #FFFFFF;
+    color: #252525;
+    height: 2.8rem;
+    border-radius: 5px;
+    padding: 0rem 1rem;
+    border-color: #C8C8C8;
+  }
+
+  @media (max-width: 768px) {
+    padding: 3rem 1rem 1rem 1rem;
+  }
+
+  @media (max-width: 500px) {
+    flex-direction: column;
+  }
 
   span { 
     color: #252525;
@@ -45,21 +92,39 @@ export const ContentHeader = styled.div`
 
 `;
 
-export const Card = styled.div`
+export const ContentCard = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(500px, auto));
   margin: 0rem 3rem;
-  background: #FFF;
-  width: 35.625rem;
-  height: 12.5rem;
-  border-radius: 5px;
-  padding: 1.3rem;
-  display: flex;
-  flex-direction: column;
+  gap: 1.3rem 3.5rem;
+  grid-auto-flow: row;
+
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+    margin: 0rem 1rem;
+  }
 `;
 
 export const CardTop = styled.div`
   display: flex;
   justify-content: space-between;
   margin-bottom: 1.4rem;
+
+  svg {
+    color: #F9B036;
+  }
+
+  div {
+    display: none;
+  }
+
+  button {
+    &:first-child {
+      margin-right: 1rem;
+    }
+    font-size: 0px;
+    background-color: transparent;
+  }
 `;
 
 export const CardTitle = styled.div`
@@ -107,16 +172,154 @@ export const Divider = styled.div`
 
 export const CardDescription = styled.div`
   color: #AEAEAE;
+
+  p {
+    text-overflow: ellipsis;
+    overflow: hidden;
+    white-space: nowrap;
+  }
 `;
 
-export const Tag = styled.div`
+export const Tag = styled.div<TagProps>`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: #11A171;
+  ${props => props.status === 2 && css`
+    background-color: #DB4663;
+  `}
+  ${props => props.status === 1 && css`
+    background-color: #11A171;
+  `}
   padding: 0rem 1rem;
   border-radius: 10px;
   color: #FFF;
   font-size: 0.75rem;
   margin-left: 0.75rem;
-`
+`;
+
+export const Card = styled.div`
+  opacity: 0;
+  @keyframes up {
+    from {
+      opacity: 0;
+      transform: translateY(15px);
+    }
+    to {
+      opacity: 1;
+    }
+  }
+  animation: up 350ms ease-out forwards;
+
+  background: #FFF;
+  height: 12.5rem;
+  border-radius: 5px;
+  padding: 1.3rem;
+  display: flex;
+  flex-direction: column;
+
+  position: relative;
+
+  &:hover {
+    transition: 1s;
+    background-color: #F9B036;
+    ${CardTop} {
+      svg {
+        color: #252525;
+      }
+      div {
+        display: block;
+      }
+    }
+
+    ${CardTitle} {
+      display: none;
+    }
+    ${Divider} {
+      display: none;
+    }
+    ${CardDescription} {
+      display: none;
+    }
+    .cardHoverDescription { 
+      display: block;
+      font-weight: 500;
+    }     
+  }
+
+  .cardHoverDescription {
+    display: none;
+  }
+`;
+
+export const Loading = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  width: 100%;
+`;
+
+interface ModalProps {
+  checked: boolean;
+}
+
+export const ModalStyled = styled(Modal) <ModalProps>`
+  width: 100%;
+  max-width: 600px;
+  
+  h2 {
+    color: #252525;
+    margin-bottom: 1rem;
+  }
+
+  .buttonStatus {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between; 
+
+    .activeButton {
+      border: 1px solid #11A171;
+      color: #252525;
+
+      transition: background-color .3s ease-in;
+
+      ${props => props.checked && css`
+        background-color: #11A171;
+        color: #FFFFFF;
+      `}
+      &:hover {
+        background-color: #11A171;
+        color: #FFFFFF;
+      }
+    }
+
+    .inactiveButton {
+      border: 1px solid #DB4663;
+      color: #252525;
+
+      transition: background-color .3s ease-in;
+
+      ${props => props.checked !== true && css`
+        background-color: #DB4663;
+        color: #FFFFFF;
+      `}
+      &:hover {
+        background-color: #DB4663;
+        color: #FFFFFF;
+      }
+    }
+
+    button {
+      width: 50%;
+      height: 3.125rem;
+      margin-bottom: 1.4rem;
+      border-radius: 5px;
+      color: #FFFFFF;
+      font-weight: 500;
+      &:first-child {
+        margin-right: 1rem;
+      }
+    }
+  }
+`;
