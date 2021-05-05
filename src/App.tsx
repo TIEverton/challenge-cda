5b7b86f859c87cfa1caa5e6238a097ce0689d456
evertonpinheiroti <evertonpinheiroti@gmail.com>
2021-05-05 19:53:57 -0300

    :sparkles: Updating App

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
 import React from 'react';
+import { Provider } from 'react-redux'
+
+import store from './store';
 
 import { Routes } from './routes';
 import GlobalStyle from './styles/global';
+
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Routes />
+      <Provider
+        store={store}
+      >
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+        <Routes />
+      </Provider>
     </>
   );
 }
